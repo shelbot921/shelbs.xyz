2f319e71ad129386e0ab7f0048d20110705aaa3c
shelbot921 <61008399+shelbot921@users.noreply.github.com>
2020-02-22 17:12:31 +1100

    Change back to Shelbs version

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,11 @@
 var game;
+// create a new game instance 600px wide and 450px tall:
+game = new phaser.game(600, 450, phaser.AUTO, '');
 
-game = new Phaser.Game(600, 450, Phaser.AUTO, '');
-
+//First parameter is how our state will be called
+// second parameter is an object cotaining the needed methods for state functionality
 game.state.add('Menu', Menu);
-game.state.add('Game', Game);
+game.state.add('Game', Game),
+
+game.state.start.('Menu');
 game.state.add('Game_Over', Game_Over);
-
-
-game.state.start('Menu');
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,23 +1,19 @@
 var Menu = {
+  preload : function() {
+    game.load.image('menu', '.assets/images/menu.png') ;
+},
 
-    preload : function() {
-        // Load all the needed resources for the menu.
-        game.load.image('menu', './assets/images/menu.png');
-    },
+create: function () {
 
-    create: function () {
+//create: function () {
+//  this.add.sprite(0, 0, 'menu');
+this.add.button(0, 0, 'menu', this.startgame, this);
 
-        // Add menu screen.
-        // It will act as a button to start the game.
-        this.add.button(0, 0, 'menu', this.startGame, this);
+},
 
-    },
+startgame: function () {
+this.state.start('Game')
 
-    startGame: function () {
-
-        // Change the state to the actual game.
-        this.state.start('Game');
-
-    }
+}
 
 };
