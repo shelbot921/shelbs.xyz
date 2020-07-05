77c52a7d1e8af57c9b38cfbd3323b0fc153e6854
shelbot921 <61008399+shelbot921@users.noreply.github.com>
2020-07-06 08:03:10 +1000

    game.js
    debugging

--- assets/js/game.old.js
+++ assets/js/game.old.js
@@ -1,61 +1,51 @@
 var snake, apple, squareSize, score, speed,
-    updateDelay, direction, new_direction,
-    addNew, cursors, scoreTextValue, speedTextValue, textStyle_Key, textStyle_Value;
+    updateDelay, direection, new_direction,
+    addNew, cursors, scoreTextValue, speedTextValue, textstyle_Key, scoreText_Value;
 
 var Game = {
 
     preload : function() {
-        // Here we load all the needed resources for the level.
-        // In our case, that's just two squares - one for the snake body and one for the apple.
-        game.load.image('snake', './assets/images/snake.png');
-        game.load.image('apple', './assets/images/apple.png');
+        game.load.image('snake','./assets'/images/snake.png');
+        game.load.image('apple'),'./assets'/images/apple.png');
     },
 
-    create : function() {
+    create : function(){
 
-        // By setting up global variables in the create function, we initialise them on game start.
-        // We need them to be globally available so that the update function can alter them.
+        snake = [];
+        apple = {};
+        squareSize = 15;
+        score = 0;
+        speed =   0;
+        updateDelay = 0;
+        direction = 'right';
+        new_direction = null;
+        addNew = false
 
-        snake = [];                     // This will work as a stack, containing the parts of our snake
-        apple = {};                     // An object for the apple;
-        squareSize = 15;                // The length of a side of the squares. Our image is 15x15 pixels.
-        score = 0;                      // Game score.
-        speed = 0;                      // Game speed.
-        updateDelay = 0;                // A variable for control over update rates.
-        direction = 'right';            // The direction of our snake.
-        new_direction = null;           // A buffer to store the new direction into.
-        addNew = false;                 // A variable used when an apple has been eaten.
+        cursors = game.input.Keyboard.createCursorKeys();
+        
+		game.stage.backgroundColor ='#061f27';
 
-        // Set up a Phaser controller for keyboard input.
-        cursors = game.input.keyboard.createCursorKeys();
-
-        game.stage.backgroundColor = '#061f27';
-
-        // Generate the initial snake stack. Our snake will be 10 elements long.
         for(var i = 0; i < 10; i++){
-            snake[i] = game.add.sprite(150+i*squareSize, 150, 'snake');  // Parameters are (X coordinate, Y coordinate, image)
-        }
+             snake[i] = game.add.sprite(150+i*squareSize, 150, 'snake');  // Parameters are (X coordinate, Y coordinate, image)
+         }
 
+         // Genereate the first apple.
+         this.generateApple();
 
-        // Genereate the first apple.
-        this.generateApple();
+         // Add Text to top of game.
+         textStyle_Key = { font: "bold 14px sans-serif", fill: "#46c0f9", align: "center" };
+         textStyle_Value = { font: "bold 18px sans-serif", fill: "#fff", align: "center" };
 
-        // Add Text to top of game.
-        textStyle_Key = { font: "bold 14px sans-serif", fill: "#46c0f9", align: "center" };
-        textStyle_Value = { font: "bold 18px sans-serif", fill: "#fff", align: "center" };
+         // Score.
+         game.add.text(30, 20, "SCORE", textStyle_Key);
+         scoreTextValue = game.add.text(90, 18, score.toString(), textStyle_Value);
+         // Speed.
+         game.add.text(500, 20, "SPEED", textStyle_Key);
+         speedTextValue = game.add.text(558, 18, speed.toString(), textStyle_Value);
 
-        // Score.
-        game.add.text(30, 20, "SCORE", textStyle_Key);
-        scoreTextValue = game.add.text(90, 18, score.toString(), textStyle_Value);
-        // Speed.
-        game.add.text(500, 20, "SPEED", textStyle_Key);
-        speedTextValue = game.add.text(558, 18, speed.toString(), textStyle_Value);
-
-    },
-
-    update: function() {
-
-        // Handle arrow key presses, while not allowing illegal direction changes that will kill the player.
+     },
+	 
+    update: function() {	 
 
         if (cursors.right.isDown && direction!='left')
         {
