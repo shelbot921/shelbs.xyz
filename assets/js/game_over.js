3c3951728547bf89e30449bcf1243d22b9cb58c2
shelbot921 <61008399+shelbot921@users.noreply.github.com>
2020-07-06 07:45:42 +1000

    Debugging
    A fullstop...

--- assets/js/game_over.js
+++ assets/js/game_over.js
@@ -7,7 +7,7 @@ var Game_Over = {
 
     create : function() {
 
-        this.add.button(0, 0,'gameover', this.startGame, this);
+        this.add.button(0, 0, 'gameover', this.startGame, this);
 
         game.add.text(235, 350, "LAST SCORE", { font: "bold 16px sans-serif", fill: "#46c0f9", align: "center"});
         game.add.text(350, 348, score.toString(), { font: "bold 20px sans-serif", fill: "#fff", align: "center" });
